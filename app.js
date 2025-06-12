e9f75b005c730765e573cf1bc96962da620a9d8d
Sourav <srvsarkar2023@gmail.com>
2025-06-13 03:00:56 +0530

    Initial commit - fresh start

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const jwt=require('jsonwebtoken');
+const recipeRouter = require('./Router/recipeRoute');
+const createError = require("http-errors");
+const { errorResponse } = require('./Controller/errorSuccessResponse');
+const authRouter = require('./Router/authRoute');
+
+require("dotenv").config();
+const app = express(); // Initialize the express app first
+app.use(express.json());
+app.use(cors());  // Enable all CORS requests (now placed after app initialization)
+
+const port = process.env.PORT || 5000;
+
+
+async function main(options = {}) {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, options);
+    console.log("Connected to DB");
+  } catch (err) {
+    console.error("Error connecting to DB", err);
+  }
+}
+
+main();
+
+
+
+app.use('/api/recipe',recipeRouter);
+app.use('/api/auth', authRouter); 
+
+// Client error handling
+app.use((req, res, next) => {
+  const err = createError(404, "Route not found");
+  next(err);
+});
+
+// Server error handling (generic error handler)
+app.use((err, req, res, next) => {
+  return errorResponse(res, {
+    statusCode: err.status || 500,
+    message: err.message || "Internal Server Error",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
